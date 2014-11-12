9d3183774f14d419075efbf91b9ace584eec44b3
ILikePenguins <yonixlevi@gmail.com>
2014-11-12 12:36:15 -0500

    updated flash cards class
    flash cards work now

--- src/com/example/hiraganaflashcards/FlashCards.java
+++ src/com/example/hiraganaflashcards/FlashCards.java
@@ -3,26 +3,37 @@ package com.example.hiraganaflashcards;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FlashCards extends FragmentActivity
 {
 	
 	HashMap <String, String> chosenItems;
+	String answer;
+	TextView tv;
+	String randomValue;
+	String randomKey;
 	protected void onCreate(Bundle savedInstanceState) 
     {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.flash_card);
        
        chosenItems= new HashMap<String,String>();
-       TextView tv = (TextView)findViewById(R.id.tvKatakana);
-       String text="";
+       tv= (TextView)findViewById(R.id.tvKatakana);
        chosenItems =  (HashMap<String, String>) getIntent().getExtras().get("selected");
-      
-       tv.setText(text);
+       
+       Button buNext = (Button)findViewById(R.id.buNext);
+       buNext.setOnClickListener(new NextButton());
+       
+       Button buShowAnswer = (Button)findViewById(R.id.buShowAnswer);
+       buShowAnswer.setOnClickListener(new ShowAnswer());
        
     }
 	
@@ -34,6 +45,40 @@ public class FlashCards extends FragmentActivity
 	    	   tokens=each_item.split(" ");
 	    	   chosenItems.put(tokens[0], tokens[1]);
 	       }
+	}
+	
+	public class NextButton implements OnClickListener
+	{
+
+		public void onClick(View arg0) 
+		{
+			flashCard();
+		}
+		
+	}
+	
+	
+	public class ShowAnswer implements OnClickListener
+	{
+
+		public void onClick(View arg0) 
+		{
+				 tv.setText(randomValue + " is " +randomKey);
+		}
+		
+	}
+	
+	
+	public void flashCard()
+	{
+		Random generator = new Random();
+		Object[] keys = chosenItems.keySet().toArray();
+		randomKey = (String)keys[generator.nextInt(keys.length)];
+		randomValue= chosenItems.get(randomKey);
+		
+		tv.setText(randomValue);
+		 
+		
 	}
 	
 	
